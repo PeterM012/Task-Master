6376aff12d6c2f23c999cffeef6b8811961c3508
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-27 22:34:49 -0700

    create get function

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,10 @@ const app = require('express').Router();
 const newId = require('newId');
 const db = require('../db/db.json')
 
-
+app.get('api/notes', (req,res) => {
+    fs.readFile('./db/db.json', (err,data) => {
+        if(err) throw(err);
+        res.send(data)
+    })
+})
 
