92a7dc6546c7065ec3444796318571a598b2264f
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-28 18:42:11 -0700

    add new comments to code

--- notes.js
+++ notes.js
@@ -1,30 +1,38 @@
+
 const noteTaker = require('express').Router();
 const uuid = require('uuid')
 const db = require('./db/db.json')
+// We import the `fs` module to enable interaction with the file system
 const fs = require('fs')
 
+//Empty array for taking in the post
 let noteInput = [];
 
+//Loop adds one or more elements to the end of an array and returns the new length of the array.
 for (const note of db){
     noteInput.push(note)
 }; 
 
+//Stringify JSON note inputs 
 noteTaker.get('/', (req,res) => {
     res.send(JSON.stringify(noteInput))
 })
 
+// POST Route for submitting notes
 noteTaker.post('/', (req,res) => {
     noteInput.push({
         id: uuid.v4(),
         title: req.body.title,
         text: req.body.text
     })
+    // File description path of the file to be written in db.json.
     fs.writeFile('./db/db.json', JSON.stringify(noteInput), (err) => {
         if(err) console.log("Error");
     })
     res.send('Note Saved!')
 })
 
+ // Iterate through the notesInput to delete `req.params.delete notes`
 noteTaker.delete('/:deleteNote', (req,res) => {
     for (let i = 0; i < noteInput.length; i++) {
         if(noteInput[i].id == req.params.deleteNote){
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
+// Sets up the Express App
 const app = express();
 const PORT = process.env.port || 3001;
 
 const htmlRoutes = require('./notes.js');
 
+// Sets up the Express app to handle data parsing
+// Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.use('/api/notes',htmlRoutes);
 
+// GET route to get to the notes // GET request
+// GET Route for retrieving all the notes and index 
 app.get('/notes', (req,res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
