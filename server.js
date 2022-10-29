79e2003e9d56e40e7d4c711e6a7d93eba454023e
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-28 20:06:07 -0700

    add updated PORT for url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 // Sets up the Express App
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT|| 3001;
 
 const htmlRoutes = require('./notes.js');
 
